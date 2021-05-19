16c8e60040470b49801426a7dce1002350830f61
Cyndi Chin <cgc95@cornell.edu>
2021-05-19 12:36:50 -0400

    Add Week1 Materials

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by cynber on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
